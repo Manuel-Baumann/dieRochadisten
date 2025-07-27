f5e7d72413077ea317f3a538bead4ac274ed34ef
Manuel Baumann <manuelbaumann99@gmx.net>
2025-07-27 11:46:46 +0200

    MBA-7: Initial commit.

---
+++ backend/src/main/java/com/manuelleanderbaumann/dieRochadisten/schach/DieRochadistenApplication.java
@@ -0,0 +1,13 @@
+package com.manuelleanderbaumann.dieRochadisten.schach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DieRochadistenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DieRochadistenApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/manuelleanderbaumann/dieRochadisten/schach/DieRochadistenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuelleanderbaumann.dieRochadisten.schach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DieRochadistenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
